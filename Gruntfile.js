d8e5c465bedede66dd29b5f2b252c0c0bb15f50d
Harrison Wallin <Harrison Wallin>
2016-12-12 20:05:48 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function (grunt) {
+  grunt.initConfig(
+    {
+      sass: {
+        dist: {
+          files: {
+            'dist/css/butter.css': 'scss/main.scss'
+          }
+        }
+      },
+      concat: {
+        js: {
+          options: {
+            separator: ';'
+          },
+          files: {}
+        }
+      },
+      uglify: {
+        js: {
+          files: {}
+        }
+      },
+      cssmin: {
+        add_banner: {
+          options: {
+            banner: '/* minified css */'
+          },
+          files: {
+            'dist/css/butter.min.css': ['dist/css/butter.css']
+          }
+        }
+      }
+    }
+  );
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask(
+    'default', [
+      'sass',
+      'concat',
+      'uglify',
+      'cssmin'
+    ]
+  );
+};
